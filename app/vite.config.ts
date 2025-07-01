19d1c63fa61e58d24e5ec0a60d82805ece0dc186
lnxdxtf <90481507+lnxdxtf@users.noreply.github.com>
2025-07-01 04:57:41 -0300

    remo useless

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import wasmPack from 'vite-plugin-wasm-pack'
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/rust_reel-grpc/',
   build: { minify: false },
-  plugins: [vue(), wasmPack(['./wasm-app'])],
+  plugins: [vue()],
   envPrefix: ['VITE_'],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
